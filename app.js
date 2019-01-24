9b18e37cb042cf3e68b7aeb975f93722c57df64a
Moath <mfalthaw@ucalgary.ca>
2019-01-24 10:29:18 +0300

    w2d5 warmup anagram detector solution

---
+++ app.js
@@ -0,0 +1,15 @@
+let wordList = ["enlists", "google", "inlets", "banana", "istlen"];
+
+function printAnagrams(wordToCheck) {
+  // split, join, sort
+  wordToCheck = wordToCheck.split('').sort().join('');
+
+  for (var word of wordList) {
+    let wordSorted = word.split('').sort().join('');
+    if (wordToCheck === wordSorted) {
+      console.log(word);
+    }
+  }
+}
+
+printAnagrams("listen");
